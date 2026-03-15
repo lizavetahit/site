8aca33f75f56145324fb388ba9a1bf9cc41b104f
Hytruk <lizavetahit@gmail.com>
2026-03-15 15:17:42 +0200

    connect render database

--- database/db.js
+++ database/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg")
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "music_platform",
-    password: "11111111",
-    port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 module.exports = pool
